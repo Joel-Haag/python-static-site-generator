85317f1a847ede3fc64eb10bd99345ca612b16ee
Joel Haag <joelohaag@gmail.com>
2022-07-19 23:35:04 +0200

    4th

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -33,7 +33,7 @@ class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        Parser.copy(path, source, dest)
+        self.copy(path, source, dest)
 
 
 class MarkdownParser(Parser):
