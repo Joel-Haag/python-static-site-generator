adc57025a95db9796622a958d4dc9d11a1e487c0
Joel Haag <joelohaag@gmail.com>
2022-07-19 21:50:38 +0200

    2nd parse

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,24 +7,25 @@ class Parser:
     extensions: List[str] = []
 
     def valid_extension(self, extension):
-        if extension in self.extensions:
-            return True
+        if self.extensions is not None:
+            if extension in self.extensions:
+                return True
         return False
 
     def parse(self, path: Path, source: Path, dest: Path):
-        raise NotImplementedError("Not implemented")
+        raise NotImplementedError
 
     def read(self, path):
-        with open(path) as file:
+        with open(path, 'r') as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext).name
-        with open(full_path) as file:
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, self.dest / path.relative_to(self.source))
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
@@ -32,5 +33,3 @@ class ResourceParser(Parser):
 
     def parse(self, path: Path, source: Path, dest: Path):
         Parser.copy(path, source, dest)
-
-
